6162d57c68f951f2839bf1c75496df19a7ca1beb
MacheteBy <Macheteby@gmail.com>
2023-11-24 01:01:17 +0300

    HW3 result

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -16,10 +16,14 @@ export const HW3 = () => {
   ]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    // setCurrentText(ЧЕГО-ТО НЕ ХВАТАЕТ);
+    setCurrentText(event.currentTarget.value)
   };
 
   const handleSave = () => {
+    let newText = [currentText, ...texts]
+    setTexts(newText)
+    console.log(newText)
+    setCurrentText('')
     // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
     // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
   };
@@ -27,28 +31,28 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
-      <button id={'hw03-button'} onClick={() => {}}>
+      <button id={'hw03-button'} onClick={() => {handleSave()}}>
         Сохранить
       </button>
 
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/* <ol id={'hw03-tasks'}>
-        ОТРИСОВАТЬ МАССИВ.map((el, index) => {}
+      <ol id={'hw03-tasks'}>
+      {texts.map((el, index) => {
           return (
            <li key={index} id={`hw03-task-${index}`}>
               {el}
             </li>
-          );
-        })
-      </ol>  */}
+      );
+    })}
+      </ol> 
     </div>
   );
 };
